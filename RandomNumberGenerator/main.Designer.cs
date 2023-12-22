b32af87cec2963186049be5f6b08f13d35109d5b
Matarwy <hassanmatarwy@gmail.com>
2023-12-22 22:18:25 +0200

    init

---
+++ RandomNumberGenerator/main.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace RandomNumberGenerator
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.seedLabel = new System.Windows.Forms.Label();
+            this.multiplierLabel = new System.Windows.Forms.Label();
+            this.incrementLabel = new System.Windows.Forms.Label();
+            this.modulusLabel = new System.Windows.Forms.Label();
+            this.numberOfIterationsLabel = new System.Windows.Forms.Label();
+            this.seedTextBox = new System.Windows.Forms.TextBox();
+            this.multiplierTextBox = new System.Windows.Forms.TextBox();
+            this.incrementTextBox = new System.Windows.Forms.TextBox();
+            this.modulusTextBox = new System.Windows.Forms.TextBox();
+            this.numberOfIterationsTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.randomNumbersTable = new System.Windows.Forms.DataGridView();
+            this.cycleLengthLabel = new System.Windows.Forms.Label();
+            this.cycleLengthTextBox = new System.Windows.Forms.TextBox();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.randomNumbersColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.randomNumbersTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // seedLabel
+            // 
+            this.seedLabel.AutoSize = true;
+            this.seedLabel.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.seedLabel.Location = new System.Drawing.Point(44, 20);
+            this.seedLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.seedLabel.Name = "seedLabel";
+            this.seedLabel.Size = new System.Drawing.Size(124, 30);
+            this.seedLabel.TabIndex = 0;
+            this.seedLabel.Text = "Seed (X0)";
+            // 
+            // multiplierLabel
+            // 
+            this.multiplierLabel.AutoSize = true;
+            this.multiplierLabel.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.multiplierLabel.Location = new System.Drawing.Point(44, 84);
+            this.multiplierLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.multiplierLabel.Name = "multiplierLabel";
+            this.multiplierLabel.Size = new System.Drawing.Size(114, 30);
+            this.multiplierLabel.TabIndex = 1;
+            this.multiplierLabel.Text = "Multiplier";
+            // 
+            // incrementLabel
+            // 
+            this.incrementLabel.AutoSize = true;
+            this.incrementLabel.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.incrementLabel.Location = new System.Drawing.Point(44, 151);
+            this.incrementLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.incrementLabel.Name = "incrementLabel";
+            this.incrementLabel.Size = new System.Drawing.Size(126, 30);
+            this.incrementLabel.TabIndex = 2;
+            this.incrementLabel.Text = "Increment";
+            // 
+            // modulusLabel
+            // 
+            this.modulusLabel.AutoSize = true;
+            this.modulusLabel.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.modulusLabel.Location = new System.Drawing.Point(44, 222);
+            this.modulusLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.modulusLabel.Name = "modulusLabel";
+            this.modulusLabel.Size = new System.Drawing.Size(105, 30);
+            this.modulusLabel.TabIndex = 3;
+            this.modulusLabel.Text = "Modulus";
+            // 
+            // numberOfIterationsLabel
+            // 
+            this.numberOfIterationsLabel.AutoSize = true;
+            this.numberOfIterationsLabel.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.numberOfIterationsLabel.Location = new System.Drawing.Point(44, 299);
+            this.numberOfIterationsLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.numberOfIterationsLabel.Name = "numberOfIterationsLabel";
+            this.numberOfIterationsLabel.Size = new System.Drawing.Size(248, 30);
+            this.numberOfIterationsLabel.TabIndex = 4;
+            this.numberOfIterationsLabel.Text = "Number Of Iterations";
+            // 
+            // seedTextBox
+            // 
+            this.seedTextBox.Location = new System.Drawing.Point(326, 12);
+            this.seedTextBox.Name = "seedTextBox";
+            this.seedTextBox.Size = new System.Drawing.Size(189, 38);
+            this.seedTextBox.TabIndex = 6;
+            // 
+            // multiplierTextBox
+            // 
+            this.multiplierTextBox.Location = new System.Drawing.Point(326, 76);
+            this.multiplierTextBox.Name = "multiplierTextBox";
+            this.multiplierTextBox.Size = new System.Drawing.Size(189, 38);
+            this.multiplierTextBox.TabIndex = 7;
+            // 
+            // incrementTextBox
+            // 
+            this.incrementTextBox.Location = new System.Drawing.Point(326, 143);
+            this.incrementTextBox.Name = "incrementTextBox";
+            this.incrementTextBox.Size = new System.Drawing.Size(189, 38);
+            this.incrementTextBox.TabIndex = 8;
+            // 
+            // modulusTextBox
+            // 
+            this.modulusTextBox.Location = new System.Drawing.Point(326, 219);
+            this.modulusTextBox.Name = "modulusTextBox";
+            this.modulusTextBox.Size = new System.Drawing.Size(189, 38);
+            this.modulusTextBox.TabIndex = 9;
+            // 
+            // numberOfIterationsTextBox
+            // 
+            this.numberOfIterationsTextBox.Location = new System.Drawing.Point(326, 291);
+            this.numberOfIterationsTextBox.Name = "numberOfIterationsTextBox";
+            this.numberOfIterationsTextBox.Size = new System.Drawing.Size(189, 38);
+            this.numberOfIterationsTextBox.TabIndex = 10;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(49, 359);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(466, 46);
+            this.generateButton.TabIndex = 11;
+            this.generateButton.Text = "Generate Random Numbers";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // randomNumbersTable
+            // 
+            this.randomNumbersTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.randomNumbersTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.randomNumbersColumn});
+            this.randomNumbersTable.Location = new System.Drawing.Point(561, 12);
+            this.randomNumbersTable.Name = "randomNumbersTable";
+            this.randomNumbersTable.RowHeadersWidth = 51;
+            this.randomNumbersTable.Size = new System.Drawing.Size(377, 462);
+            this.randomNumbersTable.TabIndex = 12;
+            // 
+            // cycleLengthLabel
+            // 
+            this.cycleLengthLabel.AutoSize = true;
+            this.cycleLengthLabel.Location = new System.Drawing.Point(44, 444);
+            this.cycleLengthLabel.Name = "cycleLengthLabel";
+            this.cycleLengthLabel.Size = new System.Drawing.Size(154, 30);
+            this.cycleLengthLabel.TabIndex = 13;
+            this.cycleLengthLabel.Text = "Cycle Length";
+            // 
+            // cycleLengthTextBox
+            // 
+            this.cycleLengthTextBox.Location = new System.Drawing.Point(326, 436);
+            this.cycleLengthTextBox.Name = "cycleLengthTextBox";
+            this.cycleLengthTextBox.ReadOnly = true;
+            this.cycleLengthTextBox.Size = new System.Drawing.Size(189, 38);
+            this.cycleLengthTextBox.TabIndex = 14;
+            // 
+            // Index
+            // 
+            this.Index.HeaderText = "Index";
+            this.Index.MinimumWidth = 6;
+            this.Index.Name = "Index";
+            this.Index.Width = 125;
+            // 
+            // randomNumbersColumn
+            // 
+            this.randomNumbersColumn.HeaderText = "Random Numbers";
+            this.randomNumbersColumn.MinimumWidth = 6;
+            this.randomNumbersColumn.Name = "randomNumbersColumn";
+            this.randomNumbersColumn.Width = 207;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 523);
+            this.Controls.Add(this.cycleLengthTextBox);
+            this.Controls.Add(this.cycleLengthLabel);
+            this.Controls.Add(this.randomNumbersTable);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.numberOfIterationsTextBox);
+            this.Controls.Add(this.modulusTextBox);
+            this.Controls.Add(this.incrementTextBox);
+            this.Controls.Add(this.multiplierTextBox);
+            this.Controls.Add(this.seedTextBox);
+            this.Controls.Add(this.numberOfIterationsLabel);
+            this.Controls.Add(this.modulusLabel);
+            this.Controls.Add(this.incrementLabel);
+            this.Controls.Add(this.multiplierLabel);
+            this.Controls.Add(this.seedLabel);
+            this.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "main";
+            this.Text = "Random Number Generator";
+            this.Load += new System.EventHandler(this.main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.randomNumbersTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label seedLabel;
+        private System.Windows.Forms.Label multiplierLabel;
+        private System.Windows.Forms.Label incrementLabel;
+        private System.Windows.Forms.Label modulusLabel;
+        private System.Windows.Forms.Label numberOfIterationsLabel;
+        private System.Windows.Forms.TextBox seedTextBox;
+        private System.Windows.Forms.TextBox multiplierTextBox;
+        private System.Windows.Forms.TextBox incrementTextBox;
+        private System.Windows.Forms.TextBox modulusTextBox;
+        private System.Windows.Forms.TextBox numberOfIterationsTextBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.DataGridView randomNumbersTable;
+        private System.Windows.Forms.Label cycleLengthLabel;
+        private System.Windows.Forms.TextBox cycleLengthTextBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn randomNumbersColumn;
+    }
+}
+
---
+++ RandomNumberGenerator/main.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace RandomNumberGenerator
+{
+    public partial class main : Form
+    {
+
+        public main()
+        {
+            InitializeComponent();
+        }
+
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            randomNumbersTable.Rows.Clear();
+            if (seedTextBox.Text.Equals(String.Empty) || multiplierTextBox.Text.Equals(String.Empty) || incrementTextBox.Equals(String.Empty) || modulusTextBox.Equals(String.Empty) || numberOfIterationsTextBox.Equals(String.Empty))
+            {
+                MessageBox.Show("Invalid Input, Please Enter Valid Input");
+            }else
+            {
+
+                double seed = double.Parse(seedTextBox.Text);
+                double multiplier = double.Parse(multiplierTextBox.Text);
+                double increment = double.Parse(incrementTextBox.Text);
+                double modulus = double.Parse(modulusTextBox.Text);
+                double iteration = double.Parse(numberOfIterationsTextBox.Text);
+
+                if (validateInput(multiplier, increment, modulus, seed))
+                {
+                    List<double> randomNumber = LCG_Generator(multiplier, increment, modulus, seed, iteration);
+                    double LongestPeriod = calculateActualPeriodLenth(randomNumber, multiplier, increment, modulus, seed);
+                    for (int i = 0; i < randomNumber.Count; i++)
+                    {
+                        randomNumbersTable.Rows.Add(i + 1, randomNumber[i]);
+                    }
+                    cycleLengthTextBox.Text = LongestPeriod.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Input, Please Enter Valid Input");
+                }
+
+
+            }
+            
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+        public bool validateInput(double multiplier, double increment, double modulus, double x0)
+        {
+            if ((modulus > 0) && (modulus > multiplier) && (modulus > increment) && (x0 < modulus))
+                return true;
+
+            return false;
+        }
+
+
+        public List<double> LCG_Generator(double multiplier, double increment, double modulus, double x0, double iteration)
+        {
+            List<double> lcg = new List<double>();
+            double seed_i = x0;
+
+            for (int i = 1; i <= iteration; i++)
+            {
+                seed_i = ((multiplier * seed_i) + increment) % modulus;
+                lcg.Add(seed_i);
+            }
+            return lcg;
+        }
+
+
+        public double calculateActualPeriodLenth(List<double> randomNumber, double multiplier, double increment, double modulus, double x0)
+        {
+            double LongestPeriod = 0;
+            double k = modulus - 1;
+            bool check1 = true, check2 = true, check3 = true;
+
+            if (IsPowerOfTwo(modulus) && (increment != 0))
+            {
+                if (IsRelativelyPrime(increment, modulus))
+                {
+                    LongestPeriod = modulus;
+                    check1 = false;
+                }
+            }
+            if (IsPowerOfTwo(modulus) && (increment == 0))
+            {
+                if (IsSeedOdd(x0) && (multiplier == (5 + 8 * k)))
+                {
+                    LongestPeriod = modulus / 4;
+                    check2 = false;
+                }
+            }
+            if (IsPrime(modulus) && (increment == 0))
+            {
+                if (IsDivisible((Math.Pow(multiplier, k) - 1), modulus))
+                {
+                    LongestPeriod = modulus - 1;
+                    check3 = false;
+                }
+            }
+            if (check1 && check2 && check3)
+            {
+                double first_element = randomNumber[0];
+                double second_element = randomNumber[1];
+                LongestPeriod++;
+                for (int i = 2; i < randomNumber.Count; i++)
+                {
+                    if (randomNumber[i] == second_element && randomNumber[i - 1] == first_element)
+                        break;
+                    else
+                        LongestPeriod++;
+                }
+            }
+
+            return LongestPeriod;
+        }
+
+
+
+        public bool IsPrime(double modulus)
+        {
+            if (modulus <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(modulus); i++)
+            {
+                if (modulus % i == 0)
+                    return false;
+
+            }
+            return true;
+        }
+
+
+        public bool IsDivisible(double a, double b)
+        {
+            double cDouble = a / b;
+            int cInteger = (int)cDouble;
+            if (cInteger == cDouble)
+                return true;
+            return false;
+        }
+
+
+        public bool IsPowerOfTwo(double num)
+        {
+            if (num == 0)
+                return false;
+            while (num != 1)
+            {
+                if (num % 2 != 0)
+                    return false;
+                num = num / 2;
+            }
+            return true;
+        }
+
+
+        public bool IsSeedOdd(double seed)
+        {
+            if (seed % 2 != 0)
+                return true;
+            return false;
+        }
+
+
+        public bool IsRelativelyPrime(double increment, double modulus)
+        {
+            double num = Math.Min(modulus, increment);
+            for (int i = 2; i <= num; i++)
+            {
+                if (IsDivisible(increment, i) && IsDivisible(modulus, i))
+                    return false;
+            }
+            return true;
+        }
+
+
+        public List<double> getPrimeNumbers(double number)
+        {
+            List<double> primeNumbers = new List<double>();
+            bool isPrime = true;
+
+            for (int i = 2; i <= number; i++)
+            {
+                for (int j = 2; j <= number; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primeNumbers.Add(i);
+                }
+                isPrime = true;
+            }
+
+            return primeNumbers;
+        }
+    }
+}
